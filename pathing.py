9a699a9f7af5806939b8a23e6861f89695ff7e8e
Caleb Hans <caleb.hansolo@gmail.com>
2024-10-06 21:05:18 -0500

    infinite loop :(
    whyyyyy

--- pathing.py
+++ pathing.py
@@ -16,7 +16,50 @@ def get_test_path():
 
 
 def get_random_path():
-    return [1,2]
+    # graph = graph_data.graph_data[0]
+    graph = graph_data.graph_data[global_game_data.current_graph_index]
+    # start at start node (index 0)
+    start = graph[0]
+    # end node is last node (index len - 1)
+    end = graph[len(graph) - 1]
+    # target is target_node
+    target = global_game_data.target_node[global_game_data.current_graph_index]
+
+    # path and current variables
+    current = start
+    past = start
+    path = []
+    path.append(start)
+    
+    # while path hasn't been found
+    targetFound = False
+    pathFound = False
+    while not pathFound:
+        # start from start, make random moves until at end, then check if target has been reached. If not, repeat process
+
+        # look at current's adjacency list, pick random node, then make that current node
+        random_index = random.randint(0, len(current[1]))
+        # make a past variable so that current doesnt become the one it was just previously at
+        real_past = past
+        past = current 
+        current = graph[current[1][random_index]]
+        # make sure current isnt backtracking by one IF it has enough space to do so
+        while current != real_past and len(current[1]) > 1:
+            # re-reandomize
+            random_index = random.randint(0, len(past[1]))
+            current = graph[current[1][random_index]]
+
+        # append current node to path
+        path.append(current)
+
+        # if target node found
+        if current == target:
+            targetFound = True
+
+        if current == end and targetFound:
+            pathFound = True
+
+    return path
 
 
 def get_dfs_path():
