cf7fc34d91ef15ae4a730c632fa0cec74d4c8c9d
Caleb Hans <caleb.hansolo@gmail.com>
2024-11-18 18:17:59 -0600

    doesnt work but finds path

--- config_data.py
+++ config_data.py
@@ -9,7 +9,7 @@ player_data = [
     ["Random", "2.png", colors.ORANGE],
     ["DFS", "3.png", colors.PURPLE],
     ["BFS", "4.png", colors.BLUE],
-#    ["Dijkstra", "5.png", colors.GREEN]
+    ["Dijkstra", "5.png", colors.GREEN]
 ]
 
 display_size_right = 400
@@ -17,4 +17,4 @@ display_size_bottom = 100
 display_size_top = 0
 display_size_left = 0
 graph_padding = 60
-player_speed = 7
+player_speed = 15
--- pathing.py
+++ pathing.py
@@ -1,6 +1,9 @@
 import graph_data
 import global_game_data
 from numpy import random
+import node
+import heapq as heap
+import math
 
 def set_current_graph_paths():
     global_game_data.graph_paths.clear()
@@ -248,7 +251,115 @@ def get_bfs_path():
 
 
 def get_dijkstra_path():
-    return [1,2]
+    # BFS, but use a priority queue
+    assert graph_data is not None
+    graph = graph_data.graph_data[global_game_data.current_graph_index]
+    # start at start node (index 0)
+    start_ind = 0
+    # end node is last node (index len - 1)
+    end_ind = len(graph) - 1
+    # target is target_node (gives target node's index)
+    target = global_game_data.target_node[global_game_data.current_graph_index]
+    assert target is not None
+
+    # path and current variables
+    path = [] # final path
+    parents = {} # dictionary of nodes as keys and their parent as values
+    parents[start_ind] = -1
+    visited = [] # list of nodes that have been visited
+    frontier = [(0, start_ind)]
+    heap.heapify(frontier)
+    visited.append(start_ind)
+    current_ind = 0
+
+    while frontier:
+        # get index of next element in the queue
+        current_ind = heap.heappop(frontier)[1]
+        # print(" goo goo gaga ",current_ind)
+        # make sure current index exists
+        assert current_ind is not None
+
+        # if current index is target, break
+        if current_ind == target:
+            break
+        
+        # go through current node's neighbors
+        for neighbor in graph[current_ind][1]:
+            # add neighbor node to queue if unvisited
+            if not neighbor in visited:
+                # add cost to neighbor (distance from current node)
+                current_x = graph[current_ind][0][0]
+                current_y = graph[current_ind][0][1]
+                current_coords = [current_x, current_y]
+                print(" rah",current_coords)
+                neighbor_x = graph[neighbor][0][0]
+                neighbor_y = graph[neighbor][0][1]
+                neighbor_coords = [neighbor_x, neighbor_y]
+                print(neighbor_coords)
+                cost = math.dist(current_coords, neighbor_coords)
+                # add node to visited
+                visited.append(neighbor)
+                heap.heappush(frontier, (cost, neighbor))
+                # add neighbor node to parents, and make current node's index its parent
+                parents[neighbor] = current_ind
+                print(parents)
+
+    # for assertion making sure each consecutive node is connected by an edge
+    edge_parents = parents.copy()
+
+    # add the path from start to target to the path array
+    path = get_path_from_parents(start_ind, target, parents)
+
+    frontier = [(0, target)]
+    heap.heapify(frontier)
+    visited = [target]
+    parents = {}
+    parents[target] = -1
+
+    # BFS from target to the end
+    while frontier:
+        # get index of next element in the queue
+        current_ind = heap.heappop(frontier)[1]
+        # make sure current index exists
+        assert current_ind is not None
+
+        # if end is reached, break
+        if current_ind == end_ind:
+            break
+
+        for neighbor in graph[current_ind][1]:
+            if not neighbor in visited:
+                # add cost to neighbor (distance from current node)
+                current_x = graph[current_ind][0][1]
+                current_y = graph[current_ind][0][0]
+                current_coords = [current_x, current_y]
+                neighbor_x = graph[neighbor][0][1]
+                neighbor_y = graph[neighbor][0][0]
+                neighbor_coords = [neighbor_x, neighbor_y]
+                cost = math.dist(current_coords, neighbor_coords)
+                # add node to visited
+                heap.heappush(frontier, (cost, neighbor))
+                visited.append(current_ind)
+                # add neighbor node to parents
+                parents[neighbor] = current_ind
+                edge_parents[neighbor] = current_ind
+                print(parents)
+
+    # add the path from target to end to the path array
+    path = path + get_path_from_parents(target, end_ind, parents)
+
+    # ASSERTIONS:
+
+    # Postcondition: Result path includes the target node
+    assert target in path
+    # Postcondition: Result path ends at the exit node
+    assert path[len(path) - 1] == end_ind
+    # Postcondition: Every pair of sequential vertices in the path are connected by an edge
+    # just check that every node in path has a parent, meaning they are connected
+    for i in range(len(path) - 1):
+        assert path[i] in edge_parents
+
+    return path
 
 
 def get_path_from_parents(start, end, parents):
